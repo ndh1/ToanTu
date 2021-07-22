e23756bfb31e3ee1cd69f6fa57cc3bb0ef69bf8f
huong <ndh1499@gmail.com>
2021-07-22 23:34:55 +0700

    test

---
+++ Bai6_ToanTu/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Bai6_ToanTu
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string strsonguyen;
+            int songuyen;
+            string ketqua;
+
+            strsonguyen = Console.ReadLine();
+            songuyen = Int32.Parse(strsonguyen);
+            ketqua = (songuyen % 2 == 0) ? "So chan" : "sole";
+            Console.WriteLine("{0} la {1}", songuyen, ketqua);
+
+        }
+    }
+}
